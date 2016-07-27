515e705408e2e363dbd481f630dfdc936c83a1b9
emilyperegrine <peregrineemily@gmail.com>
2016-07-27 01:30:36 +0100

    Added review updating

--- routes/reviews/manage.js
+++ routes/reviews/manage.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var reviewModel = require("../../models/review");
-var userModel = require("../../models/user");
+var reviewForm = require("../../forms/review/create_review");
 
 var script_sanitize = require('script_sanitize').sanitize;
 
@@ -17,6 +17,36 @@ router.get("/", utils.loginUser, function (req, res) {
     })
   } else {
     res.status(403).redirect("/");
+  }
+});
+
+router.get("/edit/:id", utils.loginUser, function(req, res) {
+  reviewModel.findById(req.params.id).then(function (review) {
+    if (review) {
+      var tParams = req.app.locals.getTemplateParams(req);
+      tParams.review = review;
+      res.render("reviews/edit", tParams);
+    }
+    else {
+      res.redirect("/reviews/manage");
+    }
+  });
+});
+
+router.post("/edit/:id", utils.loginUser, reviewForm, function(req, res) {
+  if (req.form && req.form.isValid) {
+    reviewModel.findById(req.params.id).then(function (review) {
+      review.update({
+        gameName: req.form.name,
+        reviewerId: req.form.userId,
+        content: req.form.r_content,
+        rating: req.form.rating
+      }).then(function () {
+        res.redirect("/reviews/get/" + req.params.id);
+      });
+    });
+  } else {
+    res.redirect("/reviews/edit/" + req.params.id);
   }
 });
 
